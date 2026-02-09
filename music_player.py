792beab90a1b7768f9658e6c64b2243a0528047c
Sarthak Kaushik <147770500+eSKay7@users.noreply.github.com>
2026-02-08 20:27:16 -0700

    Migrated ranking and ML training from JSON storage to SQLite

--- music_player.py
+++ music_player.py
@@ -1,12 +1,11 @@
 # Core Logic : Backend 
 import random
-import json
 import os 
 import time
-from typing import Dict, List, Optional
+from typing import List, Optional
 from ml_sample import traindata
+from database import get_connection
 
-RANKED_FILE = "rankedfile.json"
 
 playlist1 = [1,2,3,4,5,6,7,8,9,10]
 playlist2 = [101,102,103,104,105,106,107,108,109,110]
@@ -24,11 +23,6 @@ class MusicPlayer:
         self.playing: bool = False                              # To determine if something is playing
         self.playd: List[int] = []                              # List of songs played
         self.temp: int = 0                                      # Index for adding to queue
-        '''
-                                    0           1         2         3
-        ranking = {"song" : ["early skipped", "added", "played", "removed"]}
-        '''
-        self.ranking: Dict[int, List[int]] = {}                       # Dictionary storing the ranking of songs
         self.song_start_time: Optional[float] = None            # Time when current song started
         self.cur_playlist_id: Optional[str] = None
         self.model = None
@@ -36,11 +30,26 @@ class MusicPlayer:
     def get_playlist(self):
         return [{"id": pid, "name": f"Playlist {pid}", "count": len(pl)} for pid, pl in self.playlists.items()]
     
+    def add_song_data(self):
+        conn = get_connection()
+        cur = conn.cursor()
+
+        for song in (self.queue + ([self.songplaying] if self.songplaying else [])):
+            cur.execute(
+            """
+            INSERT INTO song_interactions 
+            (playlist_id, song_id, early_skipped, added, played, removed)
+            VALUES
+            (?, ?, 0, 0, 0, 0)
+            ON CONFLICT (playlist_id, song_id) DO NOTHING
+            """, (self.cur_playlist_id, song))
+        conn.commit()
+        conn.close()
+
     def start_playing(self, playlist_id: str):
         if playlist_id not in self.playlists:
             raise ValueError("Playlist doesn't exist")
         self.cur_playlist_id = str(playlist_id)
-        self.load_rank()
         self.queue = list(self.playlists[playlist_id])
         self.curindex = 0
         if self.queue:
@@ -51,10 +60,7 @@ class MusicPlayer:
             self.songplaying = None
             self.playing = False
             self.song_start_time = None
-        # Initializing the ranking dictionary with songs in queue
-        for song in (self.queue + ([self.songplaying] if self.songplaying else [])):
-            if song not in self.ranking:
-                self.ranking[song] = [0]*4
+        self.add_song_data()
         return self.get_state()      
     
     def play_or_pause(self):
@@ -77,12 +83,26 @@ class MusicPlayer:
         return self.get_state()
     
     def smart_shuffle(self):
-        if not self.model and self.ranking:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute(
+        """
+        SELECT song_id, early_skipped, added, played, removed
+        FROM song_interactions
+        WHERE playlist_id = ? 
+        """, (self.cur_playlist_id,))
+        rows = cur.fetchall()
+        conn.close()
+        ranking = {
+            song_id: [early_skipped, added, played, removed]
+            for song_id, early_skipped, added, played, removed in rows
+            }
+        if not self.model and ranking:
             self.train()
         if not self.model:
             print("Completely random shuffling playlist")
             newqueue = []
-            for song, rank in self.ranking.items():
+            for song, rank in ranking.items():
                 newqueue.append(song)
             if self.songplaying in newqueue:
                 newqueue.remove(self.songplaying)
@@ -94,7 +114,7 @@ class MusicPlayer:
             return self.get_state()
         
         scores = []
-        for song in self.ranking:
+        for song in ranking:
             score = self.predict_song_score(song)
             scores.append((song, score))
 
@@ -120,7 +140,23 @@ class MusicPlayer:
         return self.get_state()
     
     def train(self):
-        model = traindata(self.ranking)
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute(
+        """
+        SELECT song_id, early_skipped, added, played, removed
+        FROM song_interactions
+        WHERE playlist_id = ?
+        """, (self.cur_playlist_id,))
+        rows = cur.fetchall()
+        conn.close()
+        if not rows:
+            print("Data insufficient")
+        ranking = {
+            song_id: [early_skipped, added, played, removed]
+            for song_id, early_skipped, added, played, removed in rows
+        }
+        model = traindata(ranking)
         if not model:
             print("Data insufficient")
             return
@@ -128,20 +164,48 @@ class MusicPlayer:
         print("Updated the ML model")
 
     def predict_song_score(self, song):
-        if not self.model or song not in self.ranking:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute(
+        """
+        SELECT song_id, early_skipped, added, played, removed
+        FROM song_interactions
+        WHERE playlist_id = ? 
+        """, (self.cur_playlist_id,))
+        rows = cur.fetchall()
+        conn.close()
+        ranking = {
+            song_id: [early_skipped, added, played, removed]
+            for song_id, early_skipped, added, played, removed in rows
+            }
+        if not self.model or song not in ranking:
             return 0.5
-        features = [self.ranking[song]]
+        features = [ranking[song]]
         return self.model.predict_proba(features)[0][1]
 
     def skip(self, early: bool = False):
+        conn = get_connection()
+        cur = conn.cursor()
         if self.songplaying:
             if early:
-                self.ranking.setdefault(self.songplaying, [0,0,0,0])
-                self.ranking[self.songplaying][0] += 1
+                cur.execute(
+                """
+                UPDATE song_interactions
+                SET early_skipped = early_skipped + 1
+                WHERE playlist_id = ? and song_id = ?
+                """,
+                (self.cur_playlist_id, self.songplaying))
             else:
-                self.ranking.setdefault(self.songplaying, [0,0,0,0])
-                self.ranking[self.songplaying][2] += 1
+                cur.execute(
+                """
+                UPDATE song_interactions
+                SET played = played + 1
+                WHERE playlist_id = ? and song_id = ?
+                """,
+                (self.cur_playlist_id, self.songplaying))
             self.playd.append(self.songplaying)
+        conn.commit()
+        conn.close()
         self.train()
         if self.queue:   
             self.songplaying = self.queue.pop(self.curindex)
@@ -156,60 +220,40 @@ class MusicPlayer:
         return self.get_state()
     
     def add_to_queue(self, queue_song: int):
+        conn = get_connection()
+        cur = conn.cursor()
         self.queue.insert(self.temp, queue_song)
         self.temp += 1
-        if queue_song in self.ranking:
-            self.ranking[queue_song][1] += 1
-        self.save_rank()
+        cur.execute(
+        """
+        UPDATE song_interactions
+        SET added = added + 1
+        WHERE playlist_id = ? and song_id = ?
+        """,
+        (self.cur_playlist_id, self.songplaying))
+        conn.commit()
+        conn.close()
         self.train()
         return self.get_state()
     
     def remove_from_queue(self, dequeue_song: int):
         if dequeue_song in self.queue:
+            conn = get_connection()
+            cur = conn.cursor()
             self.queue.remove(dequeue_song)
-            if dequeue_song in self.ranking:
-                self.ranking[dequeue_song][3] += 1
-            self.save_rank()
+            cur.execute(
+            """
+            UPDATE song_interactions
+            SET removed = removed + 1
+            WHERE playlist_id = ? and song_id = ?
+            """,
+            (self.cur_playlist_id, self.songplaying))
+            conn.commit()
+            conn.close()
             self.train()
         else: 
             print("Song is not in queue")
         return self.get_state()
-    
-    def save_rank(self):
-        try:
-            if os.path.exists(RANKED_FILE):
-                with open(RANKED_FILE, "r") as file:
-                    try:
-                        data = json.load(file)
-                    except json.JSONDecodeError:
-                        data = {}
-            else:
-                data = {}
-            data[str(self.cur_playlist_id)] = {str(song): rank for song, rank in self.ranking.items()}
-            with open(RANKED_FILE, "w") as file:
-                json.dump(data, file, indent=4)
-        except Exception:
-            raise
-
-    def load_rank(self):
-        if os.path.exists(RANKED_FILE):
-            with open(RANKED_FILE, "r") as file:
-                try:
-                    data = json.load(file)
-                except json.JSONDecodeError:
-                    data = {}
-                newrankings = {}
-                playlistrank = data.get(str(self.cur_playlist_id), {})
-                for song, rank in playlistrank.items():
-                    try:
-                        newrankings[int(song)] = rank
-                    except Exception:
-                        continue
-                self.ranking = newrankings
-            return self.ranking
-        else:
-            self.ranking = {}
-            return self.ranking
                            
     # Created state for front end  
     def get_state(self):
